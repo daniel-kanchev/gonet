36c1397e70ad34547ad65110e4fbfe54e3b4e48d
daniel-kanchev <daniel.kanchev@adata.pro>
2021-02-25 11:18:36 +0200

    Finished Script

---
+++ README.md
@@ -0,0 +1,11 @@
+URL: https://www.gonet.ch/news
+
+    Spider name: gonet
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Notes:
---
+++ gonet/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'gonet'
+SPIDER_MODULES = ['gonet.spiders']
+NEWSPIDER_MODULE = 'gonet.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'gonet.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
